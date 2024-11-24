da168fcb2443ab9ca4023701bf937163da45e184
kkhmsg30 <kkhmsg30@naver.com>
2024-11-24 12:47:17 +0900

    0.02

--- log/main.tf
+++ log/main.tf
@@ -11,34 +11,40 @@ resource "aws_securityhub_product_subscription" "guardduty" {
   product_arn = "arn:aws:securityhub:${var.aws_region}::product/aws/guardduty"
 }
 
-resource "aws_cloudwatch_log_group" "security_logs" {
-  name = "/aws/security/logs"
-  retention_in_days = 30
+resource "aws_cloudwatch_event_rule" "guardduty_finding" {
+  name        = "guardduty-finding"
+  description = "Capture GuardDuty findings"
+
+  event_pattern = jsonencode({
+    source      = ["aws.guardduty"]
+    detail-type = ["GuardDuty Finding"]
+    detail      = {
+      severity = [4, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 
+                  5, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 
+                  6, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 
+                  7, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 
+                  8, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9]
+    }
+  })
 }
 
-resource "aws_cloudwatch_log_metric_filter" "guardduty_finding" {
-  name           = "GuardDutyFinding"
-  pattern        = "{ $.detail-type = \"GuardDuty Finding\" }"
-  log_group_name = aws_cloudwatch_log_group.security_logs.name
+resource "aws_cloudwatch_event_target" "sns" {
+  rule      = aws_cloudwatch_event_rule.guardduty_finding.name
+  target_id = "SendToSNS"
+  arn       = aws_sns_topic.security_alerts.arn
 
-  metric_transformation {
-    name      = "GuardDutyFindingCount"
-    namespace = "SecurityMetrics"
-    value     = "1"
+  input_transformer {
+    input_paths = {
+      severity            = "$.detail.severity"
+      Account_ID          = "$.detail.accountId"
+      Finding_ID          = "$.detail.id"
+      Finding_Type        = "$.detail.type"
+      region              = "$.region"
+      Finding_description = "$.detail.description"
+    }
+
+    input_template = "\"AWS <Account_ID> has a severity <severity> GuardDuty finding type <Finding_Type> in the <region> region.\"\n\"Finding Description:\"\n\"<Finding_description>. \"\n\"For more details open the GuardDuty console at https://console.aws.amazon.com/guardduty/home?region=<region>#/findings?search=id%3D<Finding_ID>\""
   }
-}
-
-resource "aws_cloudwatch_metric_alarm" "guardduty_finding_alarm" {
-  alarm_name          = "GuardDutyFindingAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "GuardDutyFindingCount"
-  namespace           = "SecurityMetrics"
-  period              = "300"
-  statistic           = "Sum"
-  threshold           = "0"
-  alarm_description   = "This alarm triggers when GuardDuty detects a finding"
-  alarm_actions       = [aws_sns_topic.security_alerts.arn]
 }
 
 resource "aws_sns_topic" "security_alerts" {
